b3c04068724f79d57c3b553f7e761382e800a5c7
Rohan <rohansuryawanshi992@gmail.com>
2023-07-15 13:14:33 +0530

    second commit

---
+++ src/main/java/com/medical/shop/dao/SubCategoryDAO.java
@@ -0,0 +1,41 @@
+package com.medical.shop.dao;
+
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import com.medical.shop.model.SubCategory;
+
+@Repository
+public class SubCategoryDAO {
+
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	public Collection<SubCategory> getAllSubCategories(){
+		
+		try {
+			
+			return entityManager.createQuery("From SubCategory", SubCategory.class).getResultList();
+			
+		} catch (Exception e) {
+			
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	@Transactional
+	public void addSubCategory(SubCategory subCategory){
+		try {
+			entityManager.persist(subCategory);
+		}
+		catch (Exception e) 
+		{
+			throw new RuntimeException(e);
+		}
+	}
+}
